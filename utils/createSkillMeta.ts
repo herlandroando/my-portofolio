736d9598a5ac6f799c4e082ee767bab26820a614
Herlandro Tribiakto <herlandrotri@gmail.com>
2024-05-16 20:11:05 +0700

    add skillset for createSkillMeta helper function

--- utils/createSkillMeta.ts
+++ utils/createSkillMeta.ts
@@ -9,12 +9,19 @@ const skill = {
   php: { label: "PHP", icon: "mdi:language-php" },
   js: { label: "Javascript", icon: "mdi:language-javascript" },
   ts: { label: "Typescript", icon: "mdi:language-typescript" },
+  codeigniter3: { label: "CodeIgniter 3", icon: "mdi:fire" },
+  codeigniter4: { label: "CodeIgniter 4", icon: "mdi:fire" },
+  cpp: { label: "", icon: "mdi:language-cpp" },
+  gameDevelopment: { label: "Game Development", icon: "mdi:gamepad-variant" },
+  webDevelopment: { label: "Web Development", icon: "mdi:web" },
+  godot: { label: "Godot", icon: "simple-icons:godotengine" },
+  gdScript: { label: "GD Script", icon: "simple-icons:godotengine" },
 };
 
 export default function (name: string): { label: string; icon: string } {
   const keys = Object.keys(skill);
   if (keys.includes(name)) {
-    return skill[name];
+    return skill[name as keyof typeof skill];
   }
-  return { label: _.capitalize(name), icon: "" };
+  return { label: name, icon: "" };
 }
